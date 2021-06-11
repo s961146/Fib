650cc44967ccf9fb564e993e063b0305c9bca70b
Maxwell Flitton <maxwellflitton@maxwells-mbp.mynet>
2021-06-11 01:25:45 +0100

    adding commandline

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'fib-number = flitton_fib_py.fib_calcs.fib_number:fib_numb',
+            'fib-number = flitton_fib_py.cmd.fib_numb:fib_numb',
         ],
     },
     python_requires='>=3',
